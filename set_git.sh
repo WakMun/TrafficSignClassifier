22ac37306f968efca25acbae3a0e61a7c4839a5e
Wakmun <Wak@udacityServer>
2021-09-05 01:56:26 +0000

    Model v1 working as per requirements and classfying with accuracy of over 95%

--- set_git.sh
+++ set_git.sh
@@ -6,9 +6,9 @@ git pull
 echo
 
 # Ask the user for login details
-read -p 'Git repository url: ' upstreamVar
-read -p 'Git Username: ' userVar
-read -p 'Git email: ' emailVar
+upstreamVar='https://github.com/WakMun/TrafficSignClassifier.git'
+userVar='Wakmun'
+emailVar='Wak@udacityServer'
 
 echo
 echo Thank you $userVar!, we now have your credentials
